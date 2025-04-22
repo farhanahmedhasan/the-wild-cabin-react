d1fd66c16abcc90820980dc039830ac7e6273720
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-04-23 03:00:17 +0600

    added index redirect to login page

--- src/routes/landingRoutes.tsx
+++ src/routes/landingRoutes.tsx
@@ -1,8 +1,12 @@
-import { RouteObject } from 'react-router'
+import { Navigate, RouteObject } from 'react-router'
 
 import AuthLogin from '@/pages/auth/AuthLogin'
 
 const landingRoutes: RouteObject[] = [
+  {
+    index: true,
+    element: <Navigate to="/login" replace />
+  },
   {
     path: '/login',
     element: <AuthLogin />
