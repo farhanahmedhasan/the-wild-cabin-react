1c5ff01eb176c3e535006b113efef48492d3bb3e
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-04-24 15:34:04 +0600

    cleanup pages

--- src/pages/dashboards/DashboardCabins.tsx
+++ src/pages/dashboards/DashboardCabins.tsx
@@ -1,15 +1,3 @@
-import { useEffect } from 'react'
-
-import { getCabins } from '@/services/apiCabins'
-
 export default function DashboardCabins() {
-  useEffect(() => {
-    const fetchCabins = async () => {
-      const cabins = await getCabins()
-      console.log(cabins)
-    }
-
-    fetchCabins()
-  }, [])
   return <div>DashboardCabins</div>
 }
