0779583399d77d95eb632519ac5d1c58fdafd7b1
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-04-24 18:21:31 +0600

    added tanstack query and devtool set them up cleaned main.tsx and refactored app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { BrowserRouter } from 'react-router'
 import { useRoutes } from 'react-router'
 
 import routes from '@/routes/routes'
 
+const queryClient = new QueryClient()
+
 function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+function Routes() {
   return useRoutes(routes)
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 import { StrictMode } from 'react'
 
 import App from './App.tsx'
 import '@/index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
