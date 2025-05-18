52516baa9e112a438deae1da39c7c7221fbe24c5
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-05-19 01:16:55 +0600

    added query params on cabins discount

--- src/components/filters/Filter.tsx
+++ src/components/filters/Filter.tsx
@@ -1,3 +1,5 @@
+import { useSearchParams } from 'react-router'
+
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/Select'
 
 interface IProps {
@@ -6,8 +8,22 @@ interface IProps {
 }
 
 export default function Filter(props: IProps) {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  function handleChange(val: string) {
+    props.onChange(val)
+
+    if (val === 'all') searchParams.delete('discount')
+
+    if (val !== 'all') {
+      searchParams.set('discount', val)
+    }
+
+    setSearchParams(searchParams)
+  }
+
   return (
-    <Select value={props.value} onValueChange={(val) => props.onChange(val)}>
+    <Select value={props.value} onValueChange={handleChange}>
       <SelectTrigger className="w-[200px]">
         <SelectValue placeholder="Filter by discount" />
       </SelectTrigger>
