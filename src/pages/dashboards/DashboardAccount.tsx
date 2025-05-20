77752f9e6703d151fd3ff8e657215cdc7e7f1fc3
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-05-20 14:15:11 +0600

    removed accounts and added other page folder

--- src/pages/dashboards/DashboardAccount.tsx
+++
@@ -1,3 +0,0 @@
-export default function DashboardAccount() {
-  return <div>DashboardAccount</div>
-}
--- src/routes/dashboardRoutes.tsx
+++ src/routes/dashboardRoutes.tsx
@@ -2,11 +2,11 @@ import { Navigate, RouteObject } from 'react-router'
 
 import DashboardSettings from '@/pages/dashboards/settings/DashboardSettingsIndex'
 import DashboardCabins from '@/pages/dashboards/cabins/DashboardCabinIndex'
-import DashboardBookings from '@/pages/dashboards/DashboardBookings'
-import DashboardAccount from '@/pages/dashboards/DashboardAccount'
+import DashboardBookings from '@/pages/dashboards/bookings/DashboardBookings'
+
 import DashboardLayout from '@/layout/dashboard/DashboardLayout'
-import DashboardUsers from '@/pages/dashboards/DashboardUsers'
-import DashboardHome from '@/pages/dashboards/DashboardHome'
+import DashboardUsers from '@/pages/dashboards/users/DashboardUsers'
+import DashboardHome from '@/pages/dashboards/home/DashboardHome'
 
 const dashboardRoutes: RouteObject[] = [
   {
@@ -36,10 +36,6 @@ const dashboardRoutes: RouteObject[] = [
       {
         path: 'settings',
         element: <DashboardSettings />
-      },
-      {
-        path: 'account',
-        element: <DashboardAccount />
       }
     ]
   }
